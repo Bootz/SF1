73ebe47ef64e1d87d8368c892c9763f52872127d
Bootz <bootz@projectskyfire.org>
2012-03-14 11:33:57 -0500

    Core/Maps: Moved Maps Off of Trinity::Singleton and onto ACE - Also adjusted Maps to use Ace threading - just more preps for removing Shared/Polices.
    Signed-off-by: Bootz <bootz@projectskyfire.org>

--- src/server/game/Server/Protocol/Handlers/PoolHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/PoolHandler.cpp
@@ -249,7 +249,7 @@ bool PoolGroup<Creature>::Spawn1Object(uint32 guid)
         sObjectMgr.AddCreatureToGrid(guid, data);
 
         // Spawn if necessary (loaded grids only)
-        Map* map = const_cast<Map*>(MapManager::Instance().CreateBaseMap(data->mapid));
+        Map* map = const_cast<Map*>(sMapMgr.CreateBaseMap(data->mapid));
         // We use spawn coords to spawn
         if (!map->Instanceable() && map->IsLoaded(data->posX, data->posY))
         {
@@ -277,7 +277,7 @@ bool PoolGroup<GameObject>::Spawn1Object(uint32 guid)
         sObjectMgr.AddGameobjectToGrid(guid, data);
         // Spawn if necessary (loaded grids only)
         // this base map checked as non-instanced and then only existed
-        Map* map = const_cast<Map*>(MapManager::Instance().CreateBaseMap(data->mapid));
+        Map* map = const_cast<Map*>(sMapMgr.CreateBaseMap(data->mapid));
         // We use current coords to unspawn, not spawn coords since creature can have changed grid
         if (!map->Instanceable() && map->IsLoaded(data->posX, data->posY))
         {
